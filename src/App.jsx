21e9de6bc5305b0c298927d25483b18137870926
spezdmtest <spezd.m.@gmail.com>
2024-04-05 17:31:12 +0300

    add new component WayToTeach

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,45 @@
-import Header from "./components/Header"
+import Header from "./components/Header";
+import {ways} from "./data"
+
+function WayToTeach(props) {
+  console.log(props);
+  return (
+    <li>
+      <p>
+        <strong>Фільтрація інформації та технологій. </strong>З величезної
+        кількості інформації та навичок в ІТ ви отримуєте тільки те, що
+        необхідно і працює.
+      </p>
+    </li>
+  );
+}
+
 export default function App() {
   return (
     <div>
       <Header />
       <main>
-        <h1>Hello React!!!</h1>
+        <section>
+          <h3>Наш підхід до навчання</h3>
+          <ul>
+            <WayToTeach title = "111" description = "222"/>
+            <WayToTeach title = "333" description = "444"/>
+            {/* <li>
+              <p>
+                <strong>Фільтрація інформації та технологій. </strong>З
+                величезної кількості інформації та навичок в ІТ ви отримуєте
+                тільки те, що необхідно і працює.
+              </p>
+            </li>
+            <li>
+              <p>
+                <strong>Формат навчання. </strong>У той час як інші пропонують
+                50 сторінок тексту та 2-годинні вебінари, ми даємо вам стільки ж
+                знань в одному ретельно продуманому відео за 15 хвилин.
+              </p>
+            </li> */}
+          </ul>
+        </section>
       </main>
     </div>
   );
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -8,7 +8,7 @@ export default function Header() {
         alt={"Result"}
       />
       {/* <h3>Result University</h3> */}
-      <span>Час зараз: {now.toLocaleTimeString()}</span>
+      <span>Время сейчас: {now.toLocaleTimeString()}</span>
     </header>
   );
 }
