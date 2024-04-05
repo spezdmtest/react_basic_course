15a83064eaa5d975d707b3bd24cf5faa89ab63ab
spezdmtest <spezd.m.@gmail.com>
2024-04-05 16:10:01 +0300

    add logo

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,14 @@
+import logo from "/logo-name.svg";
 export default function Header() {
+  const now = new Date();
   return (
     <header>
-      <h3>Result University</h3>
-      <span>Тут будет время</span>
+      <img
+        src={logo}
+        alt={"Result"}
+      />
+      {/* <h3>Result University</h3> */}
+      <span>Час зараз: {now.toLocaleTimeString()}</span>
     </header>
   );
 }
