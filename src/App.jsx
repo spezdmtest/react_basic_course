1c70644403af06624b759f3ccb0d7714289e413f
spezdmtest <spezd.m.@gmail.com>
2024-04-16 14:49:25 +0300

    print component WayToTeach

--- src/App.jsx
+++ src/App.jsx
@@ -10,13 +10,16 @@ export default function App() {
         <section>
           <h3>Наш підхід до навчання</h3>
           <ul>
-            <WayToTeach
+            {ways.map((way) => (
+              <WayToTeach key={way.title} {...way} />
+            ))}
+            {/* <WayToTeach
               title={ways[0].title}
               description={ways[0].description}
             />
             <WayToTeach {...ways[1]} />
             <WayToTeach {...ways[2]} />
-            <WayToTeach {...ways[3]} />
+            <WayToTeach {...ways[3]} /> */}
           </ul>
         </section>
         <section>
