b92592ba4b3f79c12e8eb56f0c850cc311590420
spezdmtest <spezd.m.@gmail.com>
2024-04-22 17:29:58 +0300

    refactoring

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
 import Header from "./components/Header";
-import { ways, differences } from "./data";
-import WayToTeach from "./components/WayToTeach";
+import { differences } from "./data";
 import Button from "./components/Button/Button";
 import { useState } from "react";
+import TeachingSection from "./components/TeachingSection";
 
 export default function App() {
   const [contentType, setContentType] = useState(null);
@@ -23,43 +23,42 @@ export default function App() {
   //   tabContent = <p>Нажми на кнопку</p>
   // }
 
-
   return (
     <div>
       <Header />
       <main>
-        <section>
-          <h3>Наш підхід до навчання</h3>
-          <ul>
-            {ways.map((way) => (
-              <WayToTeach
-                key={way.title}
-                {...way}
-              />
-            ))}
-          </ul>
-        </section>
+        <TeachingSection />
         <section>
           <h3>Чим ми відрізняємося</h3>
-          <Button isActive= {contentType === "way"} onClick={() => handleClick("way")}>Подход</Button>
-          <Button isActive= {contentType === "easy"} onClick={() => handleClick("easy")}>Доступность</Button>
-          <Button isActive= {contentType === "program"} onClick={() => handleClick("program")}>Концентрация</Button>
-          
+          <Button
+            isActive={contentType === "way"}
+            onClick={() => handleClick("way")}>
+            Подход
+          </Button>
+          <Button
+            isActive={contentType === "easy"}
+            onClick={() => handleClick("easy")}>
+            Доступность
+          </Button>
+          <Button
+            isActive={contentType === "program"}
+            onClick={() => handleClick("program")}>
+            Концентрация
+          </Button>
+
           {/* v1 */}
           {/* {contentType ? (
             <p>{differences[contentType]}</p>
           ) : (
             <div>Нажми на кнопку</div>
           )} */}
-          
+
           {/* v2 */}
           {/* {!contentType ? <p>Нажми на кнопку</p> : null}
           {contentType ? <p>differences[contentType]</p> : null} */}
-          
-         
+
           {!contentType && <p>Нажми на кнопку</p>}
           {contentType && <p>{differences[contentType]}</p>}
-          
 
           {/* v4 tabContent */}
           {/* {tabContent} */}
---
+++ src/components/TeachingSection.jsx
@@ -0,0 +1,17 @@
+import WayToTeach from "./WayToTeach";
+import { ways } from "../data";
+export default function TeachingSection() {
+    return(
+        <section>
+          <h3>Наш підхід до навчання</h3>
+          <ul>
+            {ways.map((way) => (
+              <WayToTeach
+                key={way.title}
+                {...way}
+              />
+            ))}
+          </ul>
+        </section>
+    )
+}
